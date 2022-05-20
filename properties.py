628dbdf5ad9c06a246fd65f44772f31ebe127079
Juanjo <juan.mochiesp@gmail.com>
2022-05-20 18:35:03 +0200

    Create properties.py

--- properties.py
+++ properties.py
@@ -1,8 +1,8 @@
 METEO_VARS = ["tp", "2d", "2t", "sp", "10v", "10u"]
-RELATIVE_PATH = "~/Desktop/auto_meteo_python_cache/"
+RELATIVE_PATH = "~/curry-grib-runner/"
 GRIB_BIN_RELATIVE_PATH = "~/Desktop/eccodes_installation/bin"
 GRIB_LS_COMMAND = "/grib_ls"
 DEFAULT_PARAMS = ["shortName", "dataDate", "dataTime", "step"]
-PATH_TO_GRIB_FILE = "~/Downloads/"
+PATH_TO_GRIB_FILE = "~/data/"
 HEADER_LINES_SKIP = 1
 TAIL_LINES_SKIP = 11
